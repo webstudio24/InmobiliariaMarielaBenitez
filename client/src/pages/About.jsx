7f0cf4f20ab9172458709b5381303d2a6317bed4
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-17 16:03:47 -0300

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -19,13 +19,13 @@ export default function About() {
         <li className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facilis sapiente sint quidem ab suscipit.</li>
         <li className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas facilis sapiente sint quidem ab suscipit.</li>
       </ul>
-      <Link to={
+       <Link to={
                 "https://wa.me/+5493516308388"}
               className={""}
               target={"_blank"}>
-        <div className="fixed bottom-[5%] right-[2%] z-10 rounded-full w-12 h-12 flex justify-center items-center bg-green-400 cursor-pointer hover:shadow-xl">
+        <div className="fixed bottom-[10%] right-[8%] sm:right-[2%] z-10 rounded-full w-12 h-12 flex justify-center items-center bg-green-400 cursor-pointer hover:shadow-xl">
             <FaWhatsapp
-              className="text-white text-2xl"
+              className="text-white text-3xl"
 
               
             />
