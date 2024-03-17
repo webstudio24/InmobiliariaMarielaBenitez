58c3cadbf7248b0eb2f0a090065dcf9e74f38b5a
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-17 15:57:27 -0300

    Update CedulaAzul.jsx

--- client/src/pages/CedulaAzul.jsx
+++ client/src/pages/CedulaAzul.jsx
@@ -6,13 +6,13 @@ export default function CedulaAzul() {
     <div>
        <h1 className='text-3xl font-bold mb-4 text-slate-800'>Cédula Azul</h1>
        <p className='mb-4 text-slate-700'>Tranutanis cédula azul, la cual autoriza a un tercero, no propietario del vehículo, a la circulación del mismo en la vía pública.</p>
-       <Link to={
+     <Link to={
                 "https://wa.me/+5493516308388"}
               className={""}
               target={"_blank"}>
-        <div className="fixed bottom-[5%] right-[2%] z-10 rounded-full w-12 h-12 flex justify-center items-center bg-green-400 cursor-pointer hover:shadow-xl">
+        <div className="fixed bottom-[10%] right-[8%] sm:right-[2%] z-10 rounded-full w-12 h-12 flex justify-center items-center bg-green-400 cursor-pointer hover:shadow-xl">
             <FaWhatsapp
-              className="text-white text-2xl"
+              className="text-white text-3xl"
 
               
             />
