606d68dea91d05ca2a89651eb5d4d427017e6e69
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-21 09:49:31 -0300

    Update ListingItem.jsx

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,27 +4,16 @@ import { MdLocationOn } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { PiHouseLineFill } from "react-icons/pi";
 import { CgExpand } from "react-icons/cg";
-import {
-  FaBath,
-  FaBed,
-  FaChair,
-  FaMapMarkedAlt,
-  FaArrowsAltH,
-  FaWhatsapp,
-  FaSwimmingPool,
-  FaMapMarkerAlt,
-  FaPhoneAlt,
-  FaParking,
-  FaShare,
-} from "react-icons/fa";
+import { FaBath, FaBed } from "react-icons/fa";
+
 function ListingItem({ listing }) {
   return (
-    <div className="bg-red-900  shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[360px] ">
+    <div className="bg-red-900 shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[360px] ">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={listing.imageUrls[0]}
           alt="listing cover"
-          className=" h-[320px] sm:h-[220px] w-full object-cover  hover:scale-105 transition-scale duration-300"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
         <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-white text-lg font-semibold">
@@ -45,53 +34,49 @@ function ListingItem({ listing }) {
           </p>
 
           <div className="text-white flex gap-4">
-            <div className="font-bold text-xs">
-              <div className="flex flex-wrap items-center gap-6 sm:gap-2">
-                <p className="">
-                  {listing.type_property === "casa" ||
-                  listing.type_property === "dpto" ? (
-                    <FaBed />
-                  ) : (
-                    <CgExpand />
-                  )}
-                </p>
-                <p className="">
-                  {listing.type_property === "casa" ||
-                  listing.type_property === "dpto"
-                    ? listing.bedrooms > 1
-                      ? `${listing.bedrooms} habitaciones`
-                      : `${listing.bedrooms} habitación`
-                    : listing.type_property === "terreno" ||
-                      listing.type_property === "local" ||
-                      listing.type_property === "galpon"
-                    ? `${listing.mts_totales} m² totales`
-                    : "Información no disponible"}
-                </p>
-              </div>
+            <div className="font-bold text-xs flex items-center">
+              <p className="mr-1">
+                {listing.type_property === "casa" ||
+                listing.type_property === "dpto" ? (
+                  <FaBed />
+                ) : (
+                  <CgExpand />
+                )}
+              </p>
+              <p className="mr-1">
+                {listing.type_property === "casa" ||
+                listing.type_property === "dpto"
+                  ? listing.bedrooms > 1
+                    ? `${listing.bedrooms} habitaciones`
+                    : `${listing.bedrooms} habitación`
+                  : listing.type_property === "terreno" ||
+                    listing.type_property === "local" ||
+                    listing.type_property === "galpon"
+                  ? `${listing.mts_totales} m² totales`
+                  : "Información no disponible"}
+              </p>
             </div>
-            <div className="font-bold text-xs">
-            <div className="flex flex-wrap items-center gap-6 sm:gap-2">
-                <p className="">
-                  {listing.type_property === "casa" ||
-                  listing.type_property === "dpto" ? (
-                    <FaBath />
-                  ) : (
-                    <PiHouseLineFill />
-                  )}
-                </p>
-                <p className="">
-                  {listing.type_property === "casa" ||
-                  listing.type_property === "dpto"
-                    ? listing.bathrooms > 1
-                      ? `${listing.bathrooms} baños`
-                      : `${listing.bathrooms} baño`
-                    : listing.type_property === "terreno" ||
-                      listing.type_property === "local" ||
-                      listing.type_property === "galpon"
-                    ? `${listing.mts_cubiertos} m² cubiertos`
-                    : "Información no disponible"}
-                </p>
-              </div>
+            <div className="font-bold text-xs flex items-center">
+              <p className="mr-1">
+                {listing.type_property === "casa" ||
+                listing.type_property === "dpto" ? (
+                  <FaBath />
+                ) : (
+                  <PiHouseLineFill />
+                )}
+              </p>
+              <p className="mr-1">
+                {listing.type_property === "casa" ||
+                listing.type_property === "dpto"
+                  ? listing.bathrooms > 1
+                    ? `${listing.bathrooms} baños`
+                    : `${listing.bathrooms} baño`
+                  : listing.type_property === "terreno" ||
+                    listing.type_property === "local" ||
+                    listing.type_property === "galpon"
+                  ? `${listing.mts_cubiertos} m² cubiertos`
+                  : "Información no disponible"}
+              </p>
             </div>
           </div>
         </div>
