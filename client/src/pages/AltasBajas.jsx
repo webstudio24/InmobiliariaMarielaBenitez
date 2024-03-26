db1e4afe8c0f4e119e034c3a2f3113e2fb8e9ad0
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-26 19:14:26 -0300

    Update AltasBajas.jsx

--- client/src/pages/AltasBajas.jsx
+++ client/src/pages/AltasBajas.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import altasbajas from '../assets/img/altas-bajas.png';
 export default function AltasBajas() {
   return (
-    <div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
        <h1 className='text-3xl font-bold mb-4 text-slate-800'>Altas y Bajas</h1>
-      <p className='mb-4 text-slate-700'>Este trámite consta en dar de baja al propietario anterior del vehículo y proveer la alta como nuevo titular al comprador del automóvil.</p>
+      <p className='mb-4 text-slate-700'>En nuestra inmobiliaria, te ayudamos a realizar la transferencia de tu vehículo de forma rápida, segura y sin complicaciones.
+Nos encargamos de todo el papeleo y las gestiones necesarias para que no tengas que preocuparte por nada.
+</p>
+<p className='mb-4 text-slate-700'>El trámite de transferencia consta de dos pasos principales:</p>
+<p className='mb-4 text-slate-700'><b>Baja:</b> </p>
+<p className='mb-4 text-slate-700'>Baja por robo/ baja por reducción total.
+</p>
+<p className='mb-4 text-slate-700'><b>Alta: </b></p>
+<ul className='mb-4 text-slate-700'>
+  <li>Te registramos como nuevo titular del vehículo en el Registro Nacional de la Propiedad Automotor.
+</li>
+  <li>Te entregamos la nueva Cédula Verde a tu nombre.
+</li>
+</ul>
+<img src={altasbajas} alt="" className="w-50 bg-cover bg-center border rounded-lg" />
+
+
       <Link to={
                 "https://wa.me/+5493516308388"}
               className={""}
