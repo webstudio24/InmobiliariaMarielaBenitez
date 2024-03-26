cc882e370589071271f290c21737b9a617e37eb3
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-26 19:15:13 -0300

    Update Informes.jsx

--- client/src/pages/Informes.jsx
+++ client/src/pages/Informes.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import informes from '../assets/img/informes.png';
 export default function Informes() {
   return (
-    <div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>Informes</h1>
-      <p className='mb-4 text-slate-700'>Antes de concretar cualquier operación de compraventa de un vehículo deberá realizarse un informe detallado del mismo donde consten los datos de dominio, títulos y cédulas de indentificación del mismo</p>
+      <p className='mb-4 text-slate-700'>Antes de concretar cualquier operación de compraventa de un vehículo, es fundamental realizar un informe detallado del mismo que incluya los datos de dominio, títulos y cédulas de identificación. Este informe permitirá conocer la situación legal del vehículo
+</p>
+<p>También puede contactarnos a nuestro WhatsApp para obtener asesoramiento sobre este tema. <br />
+¡Esperamos su consulta!
+</p>
+<img src={informes} alt="" className="w-50 bg-cover bg-center border rounded-lg" />
      <Link to={
                 "https://wa.me/+5493516308388"}
               className={""}
