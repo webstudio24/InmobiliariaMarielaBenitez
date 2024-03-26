402e0f9adf89cd49cf795fe47a0335b9d87fd775
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-26 15:19:59 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'public' // Configuración del directorio de salida
+  },
   server: {
     proxy: {
-      '/api':{
+      '/api': {
         target: 'http://localhost:3000',
         secure: false,
-      },
-
-    },
+      }
+    }
   },
-  plugins: [react()],
-})
+  plugins: [react()]
+});
