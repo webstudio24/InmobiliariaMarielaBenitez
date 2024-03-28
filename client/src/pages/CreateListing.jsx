828dd6e92372c47b1455f70903fdaefabb1aa9e3
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-03-28 17:33:16 -0300

    Update CreateListing.jsx

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -22,7 +22,7 @@ export default function CreateListing() {
     type_property: "casa",
     bedrooms: 0,
     bathrooms: 0,
-    regularPrice: 50,
+    regularPrice: 0,
     discountedPrice: 0,
     offer: false,
     parking: false,
@@ -421,7 +421,7 @@ export default function CreateListing() {
               <input
                 type="number"
                 id="regularPrice"
-                min="50"
+                min="0"
                 max="1000000000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
