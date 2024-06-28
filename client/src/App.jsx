a0cab50330b58407d8660c3218e61cbd91bd96f7
webstudio24 <159570720+webstudio24@users.noreply.github.com>
2024-06-28 12:55:22 -0300

    update for tasaciones.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -13,6 +13,7 @@ import CedulaAzul from './pages/CedulaAzul';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Tasaciones from './pages/Tasaciones';
 
 import { useSelector } from "react-redux";
 import Listing from './pages/Listing';
@@ -22,37 +23,38 @@ import Footer from './components/Footer';
 
 export default function App() {
 
-  const { currentUser,loading,error } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   return (
-    <BrowserRouter> 
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/sign-in' element={<SignIn/>}/>
-      <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/search' element={<Search/>}/>
-      <Route path='/listing/:listingId' element={<Listing/>}/>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
 
-        
-      <Route element={<PrivateRoute/>}> 
-        <Route path='/profile' element={<Profile/>}/>
-           <Route path='/create-listing' element={<CreateListing/>}/>
- 
-          <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
-        
 
-      </Route>
-      <Route path='/transferencias' element={<Transferencias/>}/>
-      <Route path='/inscripciones' element={<Inscripciones/>}/>
-      <Route path='/informes' element={<Informes/>}/>
-      <Route path='/altas-bajas' element={<AltasBajas/>}/>
-      <Route path='/cedula-azul' element={<CedulaAzul/>}/>
-      
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
 
-    </Routes>
+          <Route path='/update-listing/:listingId' element={<UpdateListing />} />
 
-    <Footer/>
+
+        </Route>
+        <Route path='/transferencias' element={<Transferencias />} />
+        <Route path='/inscripciones' element={<Inscripciones />} />
+        <Route path='/informes' element={<Informes />} />
+        <Route path='/altas-bajas' element={<AltasBajas />} />
+        <Route path='/cedula-azul' element={<CedulaAzul />} />
+        <Route path='/tasaciones' element={<Tasaciones />} />
+
+
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
   )
 }
